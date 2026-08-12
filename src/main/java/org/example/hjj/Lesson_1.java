66d83523c7fe10e38520ca0a0af6a9bd5c5bec5a
777MARINA777 <marina_bratikova@mail.ru>
2026-08-13 01:11:45 +0300

    Lesson_1.java

--- src/main/java/org/example/hjj/Lesson_1.java
+++ src/main/java/org/example/hjj/Lesson_1.java
@@ -3,6 +3,6 @@ package org.example.hjj;
 public class Lesson_1 {
     public static void main(String[] args) {
         System.out.println("Привет");
-        System.out.println("Пока");
+        System.out.println("Покаf");
     }
 }
